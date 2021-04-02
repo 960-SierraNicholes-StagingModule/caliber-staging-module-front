979c439b022b69a545a43365c3a4a1d4b757accc
MRVentura <milo.r.v@gmail.com>
2021-04-02 14:33:33 -0600

    display manager name

--- src/app/models/manager-model/manager.ts
+++ src/app/models/manager-model/manager.ts
@@ -1,13 +1,8 @@
 export class Manager {
 
     id: number;
-    firstName: string;
-    lastName: string;
 
-    constructor(id: number,firstName?: string,
-    lastName?: string){
+    constructor(id: number){
         this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
     }
 }
