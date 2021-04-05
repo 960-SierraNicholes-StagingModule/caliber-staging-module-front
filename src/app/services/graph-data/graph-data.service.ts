24d170a64f6e8a21fd5def80778ddbe573a93e18
Kurt Werber <kurtwerber@Kurts-MBP-2.fios-router.home>
2021-04-05 17:14:27 -0400

    batch averages

--- src/app/services/graph-data/graph-data.service.ts
+++ src/app/services/graph-data/graph-data.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { SpiderData } from '../../models/spider-data/spider-data.model';
+import * as Collections from 'typescript-collections';
+import { map } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root',
@@ -15,5 +17,33 @@ export class GraphDataService {
 
     getSpiderDataByBatchAndAssociate(batchId: string, associateEmail: string): Observable<SpiderData[]> {
         return this.http.get<SpiderData[]>(this.caliberUrl + '/grades/reports/' + batchId + '/spider/' + associateEmail);
+    }
+
+    getSpiderDataByBatch(batchId: string): [string, number][] {
+        let averages = new Collections.Dictionary<string, number[]>();
+        let data = new Array();
+
+        this.http.get(this.caliberUrl + '/grades/reports/' + batchId + '/spider/').pipe(map(resp => resp as SpiderData[])).subscribe(resp => {data = resp});
+        // await this.http.get(this.caliberUrl + '/grades/reports/' + batchId + '/spider/').pipe(map(resp => resp as SpiderData[])).toPromise().then(resp => {data = resp});
+
+        data.forEach(spider => {if(!averages.containsKey(spider.assessmentType)) {
+                                    averages.setValue(spider.assessmentType, [spider.score]);
+                                } else {
+                                    averages.getValue(spider.assessmentType).push(spider.score);
+                                }
+        });
+
+        let means = new Array();
+
+        averages.forEach((key, value) => {
+            let sum = 0;
+            
+            for (let i of value) {sum += i;}
+            let mean = sum / value.length;
+            
+            means.push([key, mean]);
+        });
+
+        return means;
     }
 }
